5d9ef3c0058990f93046f5d5e10ecaf6c3b00b73
TerryIsADecentCoder <chuxiangzhou@wireless1x-155-41-67-58.bu.edu>
2016-10-07 15:29:02 -0400

    an echo server using TCP

---
+++ EchoUser.java
@@ -0,0 +1,91 @@
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.*;
+
+
+
+public class EchoUser {
+    
+    // The user socket
+    private static Socket userSocket = null;
+    // The output stream
+    private static PrintStream output_stream = null;
+    // The input stream
+    private static BufferedReader input_stream = null;
+    
+    private static BufferedReader inputLine = null;
+    
+    public static void main(String[] args) {
+        
+        // The default port.
+        int portNumber = 8000;
+        // The default host.
+       String host = "localhost";
+       String responseLine;
+        
+        if (args.length < 2) {
+            System.out.println("Usage: java User <host> <portNumber>\n"
+                             + "Now using host=" + host + ", portNumber=" + portNumber);
+        } else {
+            host = args[0];
+            portNumber = Integer.valueOf(args[1]).intValue();
+        }
+        
+        /*
+         * Open a socket on a given host and port. Open input and output streams.
+         */
+        try {
+  // YOUR CODE
+          
+         userSocket = new Socket(host, portNumber);
+         System.out.println("Just connected to a" + userSocket.getRemoteSocketAddress());        
+         output_stream=new PrintStream(userSocket.getOutputStream());
+         input_stream=new BufferedReader(new InputStreamReader(userSocket.getInputStream()));
+         inputLine=new BufferedReader(new InputStreamReader(System.in));
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + host);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to the host "
+                                   + host);
+        }
+        
+        /*
+         * If everything has been initialized then we want to send message to the
+         * socket we have opened a connection to on the port portNumber.
+         * When we receive the echo, print it out.
+         */
+         try {
+       
+         String text;
+          while ((text = inputLine.readLine()) != null) 
+            {
+            
+              output_stream.println(text); 
+          System.out.println(input_stream.readLine());
+          
+          }
+        
+  // YOUR CODE
+          
+
+                 /* Close the output stream, close the input stream, close the socket.
+                 */
+             output_stream.close();
+             input_stream.close();
+             userSocket.close();
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }    
+    }
+}
+
+
+
